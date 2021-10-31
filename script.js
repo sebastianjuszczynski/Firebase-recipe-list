759a2440d70abe4d90718e71d7cc8aeb852fa197
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-10-31 16:05:41 +0000

    time added to DOM

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const list = document.querySelector('ul');
 
 const addRecipe = (recipe) => {
+    let time = recipe.created_at.toDate();
     let html = `
         <li>
             <p>${recipe.title}</p>
+            <p>${time}</p>
         </li>
     `;
     list.innerHTML += html;
 }
 
 
-db.collection('recipes').get().then((snapshot) => {
+db.collection('recipes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         addRecipe(doc.data());
     })
