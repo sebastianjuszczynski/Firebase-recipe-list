a803210a8d4b4f7c3d8e6ceb4f61b59ede758c36
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-10-31 16:35:27 +0000

    functionality for removing recipes created

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate();
     let html = `
-        <li>
-            <p>${recipe.title}</p>
-            <p>${time}</p>
+        <li data-id="${id}">
+            <p class="my-0">${recipe.title}</p>
+            <p class="my-0">${time}</p>
+            <button class="btn btn-danger btn-sm my-2">Delete</button>
         </li>
     `;
     list.innerHTML += html;
@@ -15,7 +16,7 @@ const addRecipe = (recipe) => {
 
 db.collection('recipes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
-        addRecipe(doc.data());
+        addRecipe(doc.data(), doc.id);
     })
 }).catch(err => {
     console.log(err)
@@ -36,4 +37,13 @@ form.addEventListener("submit", (e) => {
         console.log(err)
     })
     form.reset();
-})
+});
+
+list.addEventListener("click", e => {
+    if(e.target.tagName === "BUTTON") {
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(() => {
+            
+        })
+    }
+});
