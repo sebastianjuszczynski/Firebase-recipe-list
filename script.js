a807b64985529aaf8ea424b6cb4b7f3f77a9c1c7
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-10-31 15:58:57 +0000

    created function to get data from firebase and created function to add this data to html (for now only in console)

---
+++ script.js
@@ -0,0 +1,19 @@
+const list = document.querySelector('ul');
+
+const addRecipe = (recipe) => {
+    let html = `
+        <li>
+            <p>${recipe.title}</p>
+        </li>
+    `;
+    console.log(html);
+}
+
+
+db.collection('recipes').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        addRecipe(doc.data());
+    })
+}).catch(err => {
+    console.log(err)
+});
